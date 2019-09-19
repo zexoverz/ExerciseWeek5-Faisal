38400232ded0ec1d94958b7dfc3a364d62566d4c
Faisal <aryuical@gmail.com>
2019-09-19 23:22:15 +0700

    Push

--- day-2/Exercise/kaliTerusRekursif.js
+++ day-2/Exercise/kaliTerusRekursif.js
@@ -1,11 +1,20 @@
 function kaliTerusRekursif(angka) {
+    var multiplication = helper(angka);
+    if (multiplication < 10) {
+        return multiplication;
+    } else {
+        return kaliTerusRekursif(multiplication);
+    }
+
+}
+
+function helper(angka) {
     if (angka < 10) {
         return angka;
     } else {
-        return angka % 5 * kaliTerusRekursif(parseInt(angka / 10));
-    }
+        return angka % 10 * helper(parseInt(angka / 10));
+    }​
 }
-
 // TEST CASES
 console.log(kaliTerusRekursif(66)); // 8
 console.log(kaliTerusRekursif(3)); // 3
