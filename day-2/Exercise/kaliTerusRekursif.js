1f87562157c51a444527b48b89c9b2c6f5539e57
Faisal <aryuical@gmail.com>
2019-09-19 23:47:08 +0700

    Finish Exercise

--- day-2/Exercise/kaliTerusRekursif.js
+++ day-2/Exercise/kaliTerusRekursif.js
@@ -1,22 +1,21 @@
 function kaliTerusRekursif(angka) {
-    var multiplication = helper(angka);
+    var multiplication = helper(angka); // 36 
     if (multiplication < 10) {
         return multiplication;
     } else {
         return kaliTerusRekursif(multiplication);
     }
-
 }
 
 function helper(angka) {
     if (angka < 10) {
-        return angka;
+        return angka
     } else {
-        return angka % 10 * helper(parseInt(angka / 10));
-    }​
+        return angka % 10 * kaliTerusRekursif(parseInt(angka / 10));
+    }
 }
 // TEST CASES
-console.log(kaliTerusRekursif(66)); // 8
+console.log(kaliTerusRekursif(66)); // 8   
 console.log(kaliTerusRekursif(3)); // 3
 console.log(kaliTerusRekursif(24)); // 8
 console.log(kaliTerusRekursif(654)); // 0
